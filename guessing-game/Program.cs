b22fe8a50920ff54061bc38b017fab1e70f4e417
Tiffany Smith <tsmith204@my.apsu.edu>
2022-08-22 10:22:47 -0500

    phase one

---
+++ guessing-game/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Guess the secret number: ");
+string answer = Console.ReadLine();
+int parsedAnswer = int.Parse(answer);
+Console.WriteLine($"Your guess is {parsedAnswer}");
